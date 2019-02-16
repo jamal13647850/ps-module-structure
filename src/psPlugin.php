3df5a9ebdbe4fbe83ae778e21d98539d7e71183f
Jamal Ghasemi <j.ghasemi@GoSafir.local>
2019-02-16 14:45:33 +0330

    Start

---
+++ src/psPlugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *@author Sayyed Jamal Ghasemi <https://www.linkedin.com/in/jamal1364/>
+ * Date: 02/16/2019
+ * Time: 14:40 PM
+ */
+
+namespace pgsavis\psmstructure;
+
+
+class psPlugin
+{
+
+}
