9a4794a60d6348da00802147b6a1d8713ea47aad
Jamal Ghasemi <j.ghasemi@GoSafir.local>
2019-02-16 18:59:19 +0330

    Some changes

--- src/psPlugin.php
+++ src/psPlugin.php
@@ -10,7 +10,24 @@ namespace pgsavis\psmstructure;
 
 abstract class psPlugin extends Module
 {
-    abstract function __construct();
+    public function __construct($name,$tab,$version,$author,$need_instance=0,$bootstrap=true,$displayName,$description,$confirmUninstall,$min,$max=_PS_VERSION_){
+        parent::__construct();
+        $this->name = $name; // internal identifier, unique and lowercase
+        $this->tab = $tab; // backend module coresponding category
+        $this->version = $version; // version number for the module
+        $this->author = $author; // module author
+        $this->need_instance = $need_instance; // load the module when displaying the "Modules" page in backend
+        $this->bootstrap = $bootstrap;
+
+
+
+        $this->displayName = $this->l($displayName); // public name
+        $this->description = $this->l($description); // public description
+
+        $this->confirmUninstall = $this->l($confirmUninstall); // confirmation message at uninstall
+
+        $this->ps_versions_compliancy = array('min' => $min, 'max' => $max);
+    }
     public function install($sql_file='',$hooks=[],$tabs=[]){
         // Call install parent method
         if (!parent::install())
@@ -46,5 +63,23 @@ abstract class psPlugin extends Module
 
     }
 
+    public function loadSQLFile($sql_file)
+    {
+        // Get install SQL file content
+        $sql_content = file_get_contents($sql_file);
+
+        // Replace prefix and store SQL command in array
+        $sql_content = str_replace('PREFIX_', _DB_PREFIX_, $sql_content);
+        $sql_requests = preg_split("/;\s*[\r\n]+/", $sql_content);
+
+        // Execute each SQL statement
+        $result = true;
+        foreach($sql_requests as $request)
+            if (!empty($request))
+                $result &= Db::getInstance()->execute(trim($request));
+
+        // Return result
+        return $result;
+    }
 
 }
