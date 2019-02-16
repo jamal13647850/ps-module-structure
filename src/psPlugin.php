01aff3996e8ccdcbbcbe454493b292dd86511ee1
Jamal Ghasemi <j.ghasemi@GoSafir.local>
2019-02-16 16:26:38 +0330

    Some changes
    :x

--- src/psPlugin.php
+++ src/psPlugin.php
@@ -8,7 +8,43 @@
 namespace pgsavis\psmstructure;
 
 
-interface psPlugin
+abstract psPlugin extends Module
 {
+    abstract function __construct();
+    public function install($sql_file='',$hooks=[],$tabs=[]){
+        // Call install parent method
+        if (!parent::install())
+            return false;
+
+        // Execute module install SQL statements
+        //$sql_file = dirname(__FILE__).'/install/install.sql';
+        if($sql_file){
+            if (!$this->loadSQLFile($sql_file))
+                return false;
+        }
+
+        if(!empty($hooks)){
+            foreach ($hooks as $hook){
+                if (!$this->registerHook($hook))
+                    return false;
+            }
+        }
+
+        if(!empty($tabs)){
+            foreach ($tabs as $tab){
+                // Install admin tab
+                if (!$this->installTab($tab['parent'], $tab['className'], $tab['name']))
+                    return false;
+            }
+        }
+
+        // All went well!
+        return true;
+
+    }
+    public function uninstall(){
+
+    }
+
 
 }
